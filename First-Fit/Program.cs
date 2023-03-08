ddfa2a44c24fbb6bb4e0e6ae3b30789424163069
The-DarK-os <anisri477@gmail.com>
2023-03-08 21:38:47 +0530

    Final Edits

--- First-Fit/Program.cs
+++ First-Fit/Program.cs
@@ -2,31 +2,22 @@
 
 class GFG
 {
-    // Method to allocate memory to
-    // blocks as per First fit algorithm
     static void firstFit(int[] blockSize, int m,
                          int[] processSize, int n)
     {
-        // Stores block id of the block
-        // allocated to a process
         int[] allocation = new int[n];
 
-        // Initially no block is assigned to any process
         for (int i = 0; i < allocation.Length; i++)
             allocation[i] = -1;
 
-        // pick each process and find suitable blocks
-        // according to its size ad assign to it
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
                 if (blockSize[j] >= processSize[i])
                 {
-                    // allocate block j to p[i] process
                     allocation[i] = j;
 
-                    // Reduce available memory in this block.
                     blockSize[j] -= processSize[i];
 
                     break;
@@ -47,7 +38,6 @@ class GFG
         }
     }
 
-    // Driver Code
     public static void Main()
     {
         Console.WriteLine("Enter the number of blocks");
